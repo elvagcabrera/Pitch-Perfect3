5ce78978c75291b330107ee71ac81579f6823fa2
Elva Cabrera <elvacabrera@Elvas-MacBook-Pro.local>
2015-09-05 16:39:18 -0500

    Initial Commit

---
+++ Pitch Perfect3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pitch Perfect3
+//
+//  Created by Elva Cabrera on 9/5/15.
+//  Copyright (c) 2015 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
